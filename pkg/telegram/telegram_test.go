50166bc53d251d55c1c7622f6837afbea8d93635
Gabriel Sebastian <gabriel777sh@gmail.com>
2022-12-28 14:32:31 +0700

    edit: edit test

--- pkg/telegram/telegram_test.go
+++ pkg/telegram/telegram_test.go
@@ -2,8 +2,10 @@ package telegram
 
 import (
 	"context"
+	"os"
 	"testing"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/yeyee2901/lord-bidoof-bot/pkg/config"
 	"github.com/yeyee2901/lord-bidoof-bot/pkg/datasource"
@@ -13,7 +15,11 @@ import (
 func TestGetBotStatus(t *testing.T) {
 	cfg := config.LoadConfig()
 	ds := datasource.NewDataSource(&cfg, nil, nil)
-	tg := NewTelegramService(ds)
+	bot, err := tgbotapi.NewBotAPI(os.Getenv(cfg.Telegram.TokenEnv))
+	if err != nil {
+		t.Fatal(err)
+	}
+	tg := NewTelegramService(ds, bot)
 
 	resp, err := tg.GetBotStatus(context.Background())
 	if assert.Nil(t, err) {
